ee9b94a220ee0e330c08fd3f1c1b0818f4379e6f
황준서 <1201q@naver.com>
2022-11-28 22:03:31 +0900

    URL 정리

--- src/Multi.js
+++ src/Multi.js
@@ -8,8 +8,13 @@ const Multi = () => {
   //https://velog.io/@mochapoke/TIL-netlify%EB%A1%9C-%EB%B0%B0%ED%8F%AC%EC%8B%9C-proxy-%EC%85%8B%ED%8C%85%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95
   //https://libertegrace.tistory.com/entry/Milestone-Week-3-%EB%B3%B5%EC%95%BD-%EC%A0%95%EB%B3%B4-%EC%A0%9C%EA%B3%B5-%EB%B0%8F-%EA%B4%80%EB%A6%AC
   const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
+  const URL =
+    window.location.hostname === "localhost"
+      ? `/getDrugPrdtPrmsnDtlInq01?serviceKey=BXYfYLWmyQLWjO5humu5eK%2BTjxBIjj4wR%2BB7E%2Ftbwmhi1wMWLdF204NALK%2BO1iO2LMWeeu%2BZhR2KDrsDuTcVUA%3D%3D`
+      : `/proxy?serviceKey=BXYfYLWmyQLWjO5humu5eK%2BTjxBIjj4wR%2BB7E%2Ftbwmhi1wMWLdF204NALK%2BO1iO2LMWeeu%2BZhR2KDrsDuTcVUA%3D%3D`;
   // const URL = `${PROXY}/v1/search/encyc.json`;
   const URL2 = `${PROXY}?serviceKey=BXYfYLWmyQLWjO5humu5eK%2BTjxBIjj4wR%2BB7E%2Ftbwmhi1wMWLdF204NALK%2BO1iO2LMWeeu%2BZhR2KDrsDuTcVUA%3D%3D`;
+  const URL3 = `${PROXY}/getDrugPrdtPrmsnDtlInq01?serviceKey=BXYfYLWmyQLWjO5humu5eK%2BTjxBIjj4wR%2BB7E%2Ftbwmhi1wMWLdF204NALK%2BO1iO2LMWeeu%2BZhR2KDrsDuTcVUA%3D%3D`;
   // useEffect(() => {
   //   test2();
   // }, []);
@@ -20,7 +25,7 @@ const Multi = () => {
 
   const test2 = async (parm) => {
     axios
-      .get(URL2, {
+      .get(URL, {
         params: {
           pageNo: 1,
           numOfRows: 1,
