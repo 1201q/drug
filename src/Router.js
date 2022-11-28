15e1afbb55b59c8b4e0be4e7ad0fefb4bb6ea8dc
황준서 <1201q@naver.com>
2022-11-28 20:17:18 +0900

    공공데이터 테스트(원래로 돌아가세요 다하면

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
-import App from "./App";
+// import App from "./App";
 import Multi from "./Multi";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/multi" element={<App />} />
+        {/* <Route path="/multi" element={<App />} /> */}
         <Route path="/" element={<Multi />} />
       </Routes>
     </BrowserRouter>
