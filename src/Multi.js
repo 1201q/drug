ea7e32a0af71f67992077a04de91cff971e57da9
황준서 <1201q@naver.com>
2022-11-28 20:41:28 +0900

    발상의전환2

--- src/Multi.js
+++ src/Multi.js
@@ -4,12 +4,12 @@ import React, { useEffect, useState } from "react";
 
 const Multi = () => {
   const [keyword, setKeyword] = useState("");
-  const [searchkeyword, setSearchKeyword] = useState("");
+  const [searchkeyword, setSearchKeyword] = useState("타이레놀");
   //https://velog.io/@mochapoke/TIL-netlify%EB%A1%9C-%EB%B0%B0%ED%8F%AC%EC%8B%9C-proxy-%EC%85%8B%ED%8C%85%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95
   //https://libertegrace.tistory.com/entry/Milestone-Week-3-%EB%B3%B5%EC%95%BD-%EC%A0%95%EB%B3%B4-%EC%A0%9C%EA%B3%B5-%EB%B0%8F-%EA%B4%80%EB%A6%AC
   const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
   // const URL = `${PROXY}/v1/search/encyc.json`;
-  const URL2 = `${PROXY}`;
+  const URL2 = `${PROXY}/getDrugPrdtPrmsnDtlInq01?serviceKey=BXYfYLWmyQLWjO5humu5eK%2BTjxBIjj4wR%2BB7E%2Ftbwmhi1wMWLdF204NALK%2BO1iO2LMWeeu%2BZhR2KDrsDuTcVUA%3D%3D`;
   // useEffect(() => {
   //   test2();
   // }, []);
