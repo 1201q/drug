9417bd134bb02e56e0d6705893d6967efac31cce
황준서 <1201q@naver.com>
2022-12-10 22:36:29 +0900

    로컬스토리지 버그 수정

--- src/Multi.js
+++ src/Multi.js
@@ -13,9 +13,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Multi = () => {
   const [showButton, setShowButton] = useState(false);
   const [keyword, setKeyword] = useState("");
-  const [searchkeyword, setSearchKeyword] = useState(
-    JSON.parse(localStorage.getItem("prevWord"))
-  );
+  const [searchkeyword, setSearchKeyword] = useState(() => {
+    if (JSON.parse(localStorage.getItem("prevWord")) === null) {
+      return [
+        "훼로바-유서방정",
+        "아고틴",
+        "메이트정",
+        "마그밀정",
+        "휴니즈에페리손염산염정",
+      ];
+    } else {
+      return JSON.parse(localStorage.getItem("prevWord"));
+    }
+  });
 
   //https://velog.io/@mochapoke/TIL-netlify%EB%A1%9C-%EB%B0%B0%ED%8F%AC%EC%8B%9C-proxy-%EC%85%8B%ED%8C%85%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95
   //https://libertegrace.tistory.com/entry/Milestone-Week-3-%EB%B3%B5%EC%95%BD-%EC%A0%95%EB%B3%B4-%EC%A0%9C%EA%B3%B5-%EB%B0%8F-%EA%B4%80%EB%A6%AC
